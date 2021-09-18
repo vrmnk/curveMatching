f3152c65b0cf24415502e51e883740e83348367b
M Naveen Kumar <vrmnk@hotmail.com>
2021-09-18 13:09:20 -0600

    updated curveMatcher file with new code

---
+++ curveMatcher.py
@@ -0,0 +1,18 @@
+int DTWDistance(s: array [1..n], t: array [1..m]) {
+    DTW := array [0..n, 0..m]
+
+    for i := 1 to n
+        DTW[i, 0] := infinity
+    for i := 1 to m
+        DTW[0, i] := infinity
+    DTW[0, 0] := 0
+
+    for i := 1 to n
+        for j := 1 to m
+            cost:= d(s[i], t[j])
+            DTW[i, j] := cost + minimum(DTW[i-1, j  ],    // insertion
+                                        DTW[i  , j-1],    // deletion
+                                        DTW[i-1, j-1])    // match
+
+    return DTW[n, m]
+}
