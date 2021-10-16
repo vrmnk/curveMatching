db9971df8004b734432576ca70a5f26bbe150885
M Naveen Kumar <vrmnk@hotmail.com>
2021-10-16 12:49:28 -0600

    added code for creating a canvas and drawing lines with mouseclicks

--- curveMatcher.py
+++ curveMatcher.py
@@ -1,18 +1,47 @@
-int DTWDistance(s: array [1..n], t: array [1..m]) {
-    DTW := array [0..n, 0..m]
+'''
+#create application
+from tkinter import *
 
-    for i := 1 to n
-        DTW[i, 0] := infinity
-    for i := 1 to m
-        DTW[0, i] := infinity
-    DTW[0, 0] := 0
+#create canvas
+my_window =Tk()
+my_canvas = Canvas (my_window, width=1000, height=1000, background='white')
+my_canvas.grid(row=0,column=0)
+#create line
+my_canvas.create_line(0,1000,1000,,fill='red')
+my_window.mainloop()
+'''
+import tkinter as tk
 
-    for i := 1 to n
-        for j := 1 to m
-            cost:= d(s[i], t[j])
-            DTW[i, j] := cost + minimum(DTW[i-1, j  ],    // insertion
-                                        DTW[i  , j-1],    // deletion
-                                        DTW[i-1, j-1])    // match
+def draw(event):
+    x, y = event.x, event.y
+    if canvas.old_coords:
+        x1, y1 = canvas.old_coords
+        canvas.create_line(x, y, x1, y1)
+    canvas.old_coords = x, y
 
-    return DTW[n, m]
-}
+def draw_line(event):
+
+    if str(event.type) == 'ButtonPress':
+        canvas.old_coords = event.x, event.y
+
+    elif str(event.type) == 'ButtonRelease':
+        x, y = event.x, event.y
+        x1, y1 = canvas.old_coords
+        canvas.create_line(x, y, x1, y1)
+
+def reset_coords(event):
+    canvas.old_coords = None
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, width=400, height=400)
+canvas.pack()
+canvas.old_coords = None
+
+root.bind('<ButtonPress-1>', draw_line)
+root.bind('<ButtonRelease-1>', draw_line)
+
+#root.bind('<B1-Motion>', draw)
+#root.bind('<ButtonRelease-1>', reset_coords)
+
+root.mainloop()
